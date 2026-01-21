2f49c881b16e2099ffe5dd0a918380a998e65b2f
Ari <abenaankobea1@gmail.com>
2026-01-21 15:09:55 +0000

    Fixed Lint Warning: Changed double quotes to single quotes

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
+  await dotenv.load(fileName: '.env');
   await Firebase.initializeApp(
     options: FirebaseOptions(
       apiKey: dotenv.env['FIREBASE_API_KEY'] ?? '',
